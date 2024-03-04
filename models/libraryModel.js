0e7923b39ed0a27e3b8c8d9594ba4ec4191b172b
bazhonglbj <bazhonglbj@gmail.com>
2024-03-03 22:38:31 -0800

    602library

---
+++ models/libraryModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.uwh1ozq.mongodb.net/library");
+
+const librarySchema = new mongoose.Schema({
+    bookName: String,
+    issued: Number,
+    available: Number,
+    total: Number,
+    cover: String,
+    rating: Number
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    issuedBooks: [{
+        bookName: String,
+    }],
+    numberOfIssuedBooks: Number,
+    signedIn: Boolean
+});
+
+const User = new mongoose.model("User", userSchema);
+
+const Library = new mongoose.model("Library", librarySchema);
+
+//create new book in the library
+const newBook = new Library({
+     bookName: "Diary of a Wimpy Kid: The Long Haul",
+     issued: 160,
+     available: 40,
+     total: 200,
+     cover: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg/220px-Diary_of_a_Wimpy_Kid_The_Long_Haul_book_cover.jpg",
+     rating: 5
+ });
+newBook.save();
+ console.log("newbook saved");
+
+module.exports = {Library,User};
